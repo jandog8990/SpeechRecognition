cfa0b4b3eae93d3e36cce6d4eafd84906f6cc305
Alejandro Gonzales <jandog8990@gmail.com>
2019-10-26 22:13:52 -0600

    Cleaned all comments and un-needed code from the speech filter script

--- speechFilter.py
+++ speechFilter.py
@@ -8,35 +8,29 @@ from MFCC import MFCC
 
 # Pre-emphasis
 Fs, signal = scipy.io.wavfile.read('test/s1.wav')
-#emph_signal = signal
 pre_emph = 0.95
 emph_signal = np.append(signal[0], signal[1:] - pre_emph*signal[:-1]) 
 signal_len = len(emph_signal)
 
-#s = signal[8224:8224+10]
-#print("S0 = " + str(s[0]))
-#print(s)
-#print(s[1:])
-#print(s[:-1])
-
+# Frame sizing and steps (ie overlap stride)
 frame_size = 0.025		# milliseconds
 frame_stride = 0.01 	# stride for each frame
 frame_len = 0.025*Fs	# ms to samples using Fs
 frame_step = 0.01*Fs
 
-# calculate sizes of frames
+# Calculate integer sizes of frames
 frame_len = int(round(frame_len))
 frame_step = int(round(frame_step))
 num_frames = int(np.ceil(float(np.abs(signal_len - frame_len)) / frame_step))
 
-# pad signal to make sure each frame has equal number of samples 
+# Pad signal to make sure each frame has equal number of samples 
 pad_signal_len = num_frames * frame_step + frame_len
 z = np.zeros(pad_signal_len - signal_len)
 pad_signal = np.append(emph_signal, z)	
 ar = np.arange(0, 10)
 num_frames_step = num_frames * frame_step
 
-# frames from the index and shifted index matrices
+# Frames from the index and shifted index matrices
 iMatrix1 = np.tile(np.arange(0, frame_len), (num_frames, 1))
 iMatrix2 = np.tile(np.arange(0, num_frames * frame_step, frame_step), (frame_len, 1))
 iMatrix2 = iMatrix2.T  
@@ -48,36 +42,31 @@ frames = pad_signal[iMatrix]
 N = frame_len 
 n = np.arange(0, N, 1)
 wlen = len(n)
-#N = len(n);
-#w = np.zeros(wlen)
 w = 0.54 - 0.46*np.cos((2*np.pi*n)/(N-1))
 
 # Apply Hamming window to data frames
 frames *= w
-#NFFT = 512
 
 # Compute the FFT and PSD of the spectrum
 fft_frames = np.fft.fft(frames)
 [mfft, nfft] = fft_frames.shape
 Nfft = nfft
-#fft_frames = fft_frames[:, 0:int(round(Nfft/2+1))]
 psd_frames = 1/(Fs*Nfft)*(np.abs(fft_frames)**2)
-#psd_frames[1:len(psd_frames)-1] = 2*psd_frames[1:len(psd_frames)-1]
 
 # Split the FFT frames and PSD frames in half
 psd_frames = psd_frames[:, 0:int(round(Nfft/2+1))]
 fft_frames = fft_frames[:, 0:int(round(Nfft/2+1))]
-#psd_frames = 2*psd_frames
 psd_frames = psd_frames.T
-#psd_frames -= (np.mean(psd_frames, axis=0) + 1e-8)    # normalize the psd frames
 
-# Create numpy array of frequencies and half the PSD to remove periodicity?
-# QUESTION: How to plot the frequencies of the current block
-"""
-row_sample = int(round(len(fft_frames)*2/4))	# sample at the half point of the matrix 
-sample_frames = frames[row_sample]			# row selection for FFT frames 
-mirror_psd = psd_frames[:,row_sample]	# row selection for FFT frames (for testing psd mirror
-"""
+print("Fs = " + str(Fs))
+print("Fs/2 = " + str(Fs/2))
+print("Fs/N = " + str(Fs/N))
+print("\n")
+
+print("Frames size = " + str(frames.shape))
+print("FFT Frames size = " + str(fft_frames.shape))
+print("PSD Frames size = " + str(psd_frames.shape))
+print("\n")
 
 # -------------------------------------------------------------------
 # Mel Frequency Cepstrum Coeffs - Calculations
@@ -102,63 +91,15 @@ mfcc_out = mfcc.fit()
 mel_pts = mfcc_out['mel']
 freq_pts = mfcc_out['freq']
 bin_pts = mfcc_out['bin']
-print("MFCC Output:")
-print("Mel scale (len = " + str(len(mel_pts)) + ") = " + str(mel_pts))
-print("\n")
-print("Freq pts (len = " + str(len(freq_pts)) + ") = " + str(freq_pts))
-print("\n")
-print("Bin pts (len= " + str(len(bin_pts)) + ") = " + str(bin_pts)) 
-print("\n")
 
 # Create the filterbanks and normalize from the mean
 filterbanks = mfcc.calc_filter_banks()
 
 [fm, fn] = filterbanks.shape
 #freqs = np.tile(freq, (fm,1))
-#print("Frequencies shape = " + str(freqs.shape))
-print("MFCC Filterbank (size = " + str(filterbanks.shape) + ")")
 
 # Compute the dot product between the power spectrum and filterbanks
 mfcc_fbanks = np.dot(psd_frames.T, filterbanks.T)
-
-plt.figure(1)
-for i in range(0,fm):
-    plt.plot(filterbanks[i,:])
-plt.title("MFCC Filterbank:")
-plt.show()
-
-# Frequency response of the MFCC filterbank
-#plt.imshow(10*np.log10(psd_frames), cmap=plt.cm.jet, aspect='auto');
-
-"""
-print("Frame Block Indices:")
-print(iMatrix.astype(np.int32, copy=False))
-print("\n")
-
-print("Hamming Window Frames (size = " + str(frames.shape) + "):")
-print(frames)
-print("\n")
-
-
-print("Original signal len = " + str(signal_len))
-print("Pad signal len = " + str(pad_signal_len))
-print("N = frame_len = " + str(frame_len))
-print("\n")
-"""
-
-print("Fs = " + str(Fs))
-print("Fs/2 = " + str(Fs/2))
-print("Fs/N = " + str(Fs/N))
-print("\n")
-
-print("Frames size = " + str(frames.shape))
-print("FFT Frames size = " + str(fft_frames.shape))
-print("PSD Frames size = " + str(psd_frames.shape))
-
-print("\n")
-print("Frequency size = " + str(len(freq)))
-#print("Sample frames size = " + str(len(sample_frames)))
-#print("Mirror PSD Frames size = " + str(mirror_psd.shape))
 
 # Normalize the PSD frames for better contour viewing
 psd_frames = 20*np.log10(psd_frames)
@@ -168,6 +109,11 @@ psd_frames = 20*np.log10(psd_frames)
 mfcc_fbanks = 20*np.log10(mfcc_fbanks)
 mfcc_fbanks -= (np.mean(mfcc_fbanks, axis=0) + 1e-8)
 
+plt.figure(1)
+for i in range(0,fm):
+    plt.plot(filterbanks[i,:])
+plt.title("MFCC Filterbank:")
+plt.show()
 
 plt.figure(2)
 plt.imshow(psd_frames, cmap=plt.cm.jet, aspect='auto');
@@ -180,11 +126,6 @@ plt.show()
 
 plt.figure(3)
 plt.imshow(mfcc_fbanks.T, cmap=plt.cm.jet, aspect='auto')
-'''
-plt.xticks(np.arange(0, (mfcc_fbanks.T).shape[1],
-                     int((mfcc_fbanks.T).shape[1] / 4)),
-    ['0s', '0.5s', '1s', '1.5s','2.5s','3s','3.5'])
-'''
 ax = plt.gca()
 ax.invert_yaxis()
 plt.title('PSD * MFCC Filterbank Spectrum')
